5a69f78c7113a68cc5982be0b1a068a48f9fe1e2
ArchitectUC <126648300+ArchitectUC@users.noreply.github.com>
2024-01-30 19:34:51 -0800

    upload

---
+++ nodes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World with Cloud Run and Cloud Build');
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
